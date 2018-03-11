adab6028e4c3f72cecc3633ff8abf729a3c02e13
Carlbom <haca89@gmail.com>
2018-03-11 16:54:26 +0100

    GUI functioning good enough....

--- ChessEngine.0.1/src/view/gui/ChessGui.java
+++ ChessEngine.0.1/src/view/gui/ChessGui.java
@@ -1,21 +1,12 @@
 package view.gui;
 
 import java.awt.Color;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.TextArea;
-import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.ImageObserver;
-import java.io.File;
-import java.util.Iterator;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import model.chess.rules.PieceType;
+import controller.utilities.ChessSquareInterpreter;
 
 public class ChessGui  {
 
@@ -26,9 +17,9 @@ public class ChessGui  {
 	private final int COLLUMNS = 8;
 	private final char[] xAxis = {'A', 'B', 'C', 'D', 'E', 'F', 'H', 'G'};
 	private final char[] yAxis = {'8', '7', '6', '5', '4', '3', '2', '1'};
-	
-	private String fromPos = "";
-	private String targetPos = "";
+
+	private ChessSquareButton fromPos;
+	private ChessSquareButton targetPos;
 
 	private boolean hasChosenFromPos = false;
 
@@ -42,7 +33,7 @@ public class ChessGui  {
 	 * https://stackoverflow.com/questions/21142686/making-a-robust-resizable-swing-chess-gui
 	 */
 	private void setFramesAndPanels() {
-		gui = new JFrame();
+		gui = new JFrame("Chess By Hampus");
 		gui.setSize(600, 600);
 
 		chessPanel = new JPanel();
@@ -52,12 +43,12 @@ public class ChessGui  {
 
 		createAndAddBoardButtonsToChessPanel();
 		attImagesToChessSquares();
-		
+
 		gui.add(chessPanel);
 		gui.setVisible(true);
 		gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
-	
+
 	public void attImagesToChessSquares() {
 		chessSquares[0][0].setIcon(getImage("Chess_rdt60.png"));;
 		chessSquares[1][0].setIcon(getImage("Chess_ndt60.png"));;
@@ -67,19 +58,37 @@ public class ChessGui  {
 		chessSquares[5][0].setIcon(getImage("Chess_bdt60.png"));;
 		chessSquares[6][0].setIcon(getImage("Chess_ndt60.png"));;
 		chessSquares[7][0].setIcon(getImage("Chess_rdt60.png"));;
-		
+
 		for (int i = 0; i < 8; i++) {
 			chessSquares[i][1].setIcon(getImage("Chess_pdt60.png"));
 		}
-		
+
+		for (int i = 2; i < 6; i++) {
+			for (int j = 0; j < 8; j++) {
+				chessSquares[j][i].setIconEmpty();
+			}
+		}
+
+		for (int i = 0; i < 8; i++) {
+			chessSquares[i][6].setIcon(getImage("Chess_plt60.png"));
+		}
+
+		chessSquares[0][7].setIcon(getImage("Chess_rlt60.png"));;
+		chessSquares[1][7].setIcon(getImage("Chess_nlt60.png"));;
+		chessSquares[2][7].setIcon(getImage("Chess_blt60.png"));;
+		chessSquares[3][7].setIcon(getImage("Chess_qlt60.png"));;
+		chessSquares[4][7].setIcon(getImage("Chess_klt60.png"));;
+		chessSquares[5][7].setIcon(getImage("Chess_blt60.png"));;
+		chessSquares[6][7].setIcon(getImage("Chess_nlt60.png"));;
+		chessSquares[7][7].setIcon(getImage("Chess_rlt60.png"));;
 	}
-	
+
 	public ImageIcon getImage(String imgName) {
 		return new ImageIcon(getClass().getResource("/resources/chess_icons/"+ imgName));
 	}
 
 	private void createAndAddBoardButtonsToChessPanel() {
-		ChessSquareButton b;
+		ChessSquareButton b = null;
 		for (int i = 0; i < COLLUMNS; i++) {
 			for (int j = 0; j < ROWS; j++) {
 				b = new ChessSquareButton(xAxis[j], yAxis[i]); 
@@ -89,25 +98,30 @@ public class ChessGui  {
 				else {
 					b.setBackground(Color.WHITE);
 				}
-				b.addActionListener(new ChessSquareActionListener(b.getXY())
+				b.addActionListener(new ChessSquareActionListener(b)
 				{
 					public void actionPerformed(ActionEvent e)
 					{
 						if(hasChosenFromPos) {
-							if(getXY().matches(getChosenFromPosition())) {
+							if(getButton().getXY().matches(getChosenFromPosition().getXY())) {
 								resetLocations();
 							}
 							else {
-								setTargetPosition(getXY());
-								if(sendToEngine(getChosenFromPosition(), getChosenTargetPosition()));
-								doMove();
-								resetLocations();
+								setTargetPosition(getButton());
+								if(sendToEngine(getChosenFromPosition(), getChosenTargetPosition())) {
+									doMove();
+									resetLocations();
+								}
 							}
 
 						}
 						else {
-							setFromPosition(getXY());
-							hasChosenFromPos = true;
+							if(!getButton().isEmpty()) {
+								setFromPosition(getButton());
+								hasChosenFromPos = true;
+							}
+							else
+							getButton().setSelected(false);
 						}
 
 					}
@@ -126,45 +140,55 @@ public class ChessGui  {
 
 	}
 
-	private boolean sendToEngine(String from, String to) {
-		System.out.println("Checks if valid move: " + from + " " + to);
-		//TODO implement alternative to fail if not a valid move.
-		return true;
+	private boolean sendToEngine(ChessSquareButton from, ChessSquareButton to) {
+		System.out.println("Checks if valid move: " + from.getXY() + " " + to.getXY());
+		//Ersätt TO med FROMS bild,
+		if(from.isEmpty()) {
+			return false;
+		}
+		else {
+			to.setIcon(from.getIcon());
+			from.setIconEmpty();
+			//BUGG if empty square is pressed, it will delete target image also
+			//Tilldela chess_empty till FROMS bild.
+			//TODO implement alternative to fail if not a valid move.
+			return true;
+		}
 	}
 
 	private void resetLocations() {
 		hasChosenFromPos = false;
-		setFromPosition("");
-		setTargetPosition("");
+		setFromPosition(null);
+		setTargetPosition(null);
 
 	}
 
 	public static abstract class ChessSquareActionListener implements ActionListener {
-		private String xy;
+		private ChessSquareButton button;
 
-		public ChessSquareActionListener(String square) {
-			this.xy = square;
+		public ChessSquareActionListener(ChessSquareButton button) {
+			this.button = button;
 		}
 
-		public String getXY() {
-			return xy;
+		public ChessSquareButton getButton() {
+			return button;
 		}
 
 	}
 
-	private String getChosenFromPosition() {
+	private ChessSquareButton getChosenFromPosition() {
 		return fromPos;
 	}
 
-	private void setFromPosition(String from) {
+	private void setFromPosition(ChessSquareButton from) {
 		this.fromPos = from;
 	}
 
-	private String getChosenTargetPosition() {
+	private ChessSquareButton getChosenTargetPosition() {
 		return targetPos;
 	}
 
-	private void setTargetPosition(String target) {
+	private void setTargetPosition(ChessSquareButton target) {
 		this.targetPos = target;
 	}
 
